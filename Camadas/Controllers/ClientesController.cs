9a90373a93f54f7612c0fdbfce835a429b856105
eli <elimarias@gmail.com>
2017-03-15 22:21:22 -0300

    1503

---
+++ Camadas/Controllers/ClientesController.cs
@@ -0,0 +1,56 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class ClientesController
+    {
+
+        private static List<Cliente> listaClientes = new List<Cliente>();
+
+        public void Adicionar(string nome, string cpf)
+        {
+            Cliente c = new Cliente();
+            c.Nome = nome;
+            c.Cpf = cpf;
+        }    
+        public Cliente Detalhes (int id)
+         {
+            foreach (Cliente cli in listaClientes)
+            {
+                  if (cli.ClienteId == id)
+                      return cli;
+            }
+
+             return null;
+
+          }
+
+        public void Editar (int id, string nome, string cpf)
+         {
+            foreach (Cliente cli in listaClientes)
+            {
+                if (cli.ClienteId == id)
+                {
+                    cli.Nome = nome;
+                    cli.Cpf = cpf;
+                }
+            }
+
+         }
+
+        public void Excluir(int id)
+        {
+            foreach (Cliente cli in listaClientes)
+            {
+                if (cli.ClienteId == id)
+                    listaClientes.Remove(cli);
+            }
+
+        }
+    }
+}
---
+++ Camadas/Views_WF/CadastroCliente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Views_WF
+{
+    public partial class CadastroCliente : Form
+    {
+        public CadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Salvar_Click(object sender, EventArgs e)
+        {
+            if (string)
+
+        }
+
+        private void Cancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
